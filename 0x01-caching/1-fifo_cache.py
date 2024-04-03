baaa30234069e6ad07e5750dfbeee8d33841f25f
Mahmoud Salah <ms685116@gmail.com>
2024-04-03 22:41:28 +0200

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache"""
+
+    def __init__(self):
+        """Initializes the cache."""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache."""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", first_key)
+
+    def get(self, key):
+        """Retrieves an item by key."""
+        return self.cache_data.get(key, None)
